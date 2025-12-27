ef95c94526c39511fbb6c833cfb6ebf1de55fd57
Ciaron Rider <ciaron@ridernator.com>
2025-12-27 20:33:53 +0000

    Added solution to day 3 part 1

---
+++ 03/c++/part1.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <print>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+#define INPUT "../data/inputSimple"
+
+/**
+ * Read a file entirely into a stringstream
+ *
+ * @param The name of the file to read
+ * @return The contents of the file
+ **/
+std::stringstream readFile(const std::string& filename = INPUT) {
+  std::ifstream fileStream(filename);
+  std::stringstream returnVal;
+
+  returnVal << fileStream.rdbuf();
+
+  return returnVal;
+}
+
+/**
+ * Read a file entirely into a vector of strings
+ *
+ * @param The name of the file to read
+ * @return The vector of lines in the file
+ **/
+std::vector<std::string> readFileToVector(const std::string& filename = INPUT) {
+  std::ifstream fileStream(filename);
+  std::string string;
+  std::vector<std::string> returnVal;
+
+  while (std::getline(fileStream, string)) {
+    returnVal.push_back(string);
+  }
+
+  return returnVal;
+}
+
+int main() {
+  const auto& lines = readFileToVector();
+
+  std::uint64_t total = 0;
+
+  for (const auto& line : lines) {
+    const auto& first = std::max_element(line.begin(), line.end() - 1);
+    const auto& second = std::max_element(first + 1, line.end());
+
+    total += std::stoull(line.substr(std::distance(line.begin(), first), 1)) * 10;
+    total += std::stoull(line.substr(std::distance(line.begin(), second), 1));
+  }
+
+
+  std::println("Output joltage = {}", total);
+}
---
+++ 03/readme.md
@@ -0,0 +1,1 @@
+See https://adventofcode.com/2025/day/3
