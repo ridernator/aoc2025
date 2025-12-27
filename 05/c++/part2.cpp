086c477d3f5b4c811270251fed226e171b501147
Ciaron Rider <ciaron@ridernator.com>
2025-12-27 23:29:07 +0000

    Added solution to day 5 part 2

--- 05/c++/part2.cpp
+++ 05/c++/part2.cpp
@@ -46,7 +46,6 @@ std::vector<std::string> readFileToVector(const std::string& filename = INPUT) {
 int main() {
   const auto lines = readFileToVector();
 
-  std::uint64_t total = 0;
   std::vector<std::tuple<std::uint64_t, std::uint64_t>> ranges;
 
   for (const auto& line : lines) {
@@ -60,18 +59,57 @@ int main() {
     }
   }
 
-  for (const auto& line : lines) {
-    if ((!line.empty()) && (line.find('-') == std::string::npos)) {
-      std::uint64_t num = std::stoul(line);
-      for (const auto& range : ranges) {
-        std::println("num: {}, min: {}, max: {}", num, std::get<0>(range), std::get<1>(range));
-        if ((num >= std::get<0>(range)) && (num <= std::get<1>(range))) {
-          ++total;
+  std::uint64_t total = 0;
+
+  bool simplified = true;
+
+  for (const auto& range : ranges) {
+    std::println("start: {}, stop: {}", std::get<0>(range), std::get<1>(range));
+  }
+  std::println();
+
+  while (simplified) {
+    simplified = false;
+
+    for (auto current = ranges.begin() + 1; current != ranges.end(); ++current) {
+      for (auto previous = ranges.begin(); previous != current; ++previous) {
+        if ((std::get<0>(*current) >= std::get<0>(*previous)) && (std::get<0>(*current) <= std::get<1>(*previous))) {
+          std::get<1>(*previous) = std::max(std::get<1>(*previous), std::get<1>(*current));
+
+          simplified = true;
+
+          break;
+        } else if ((std::get<1>(*current) >= std::get<0>(*previous)) && (std::get<1>(*current) <= std::get<1>(*previous))) {
+          std::get<0>(*previous) = std::min(std::get<0>(*previous), std::get<0>(*current));
+
+          simplified = true;
+
+          break;
+        } else if ((std::get<0>(*current) <= std::get<0>(*previous)) && (std::get<1>(*current) >= std::get<1>(*previous))) {
+          std::get<0>(*previous) = std::min(std::get<0>(*previous), std::get<0>(*current));
+          std::get<1>(*previous) = std::max(std::get<1>(*previous), std::get<1>(*current));
+
+          simplified = true;
 
           break;
         }
       }
+
+      if (simplified) {
+        ranges.erase(current);
+
+        for (const auto& range : ranges) {
+          std::println("start: {}, stop: {}", std::get<0>(range), std::get<1>(range));
+        }
+        std::println();
+
+        break;
+      }
     }
+  }
+
+  for (const auto& range : ranges) {
+    total += ((std::get<1>(range) - std::get<0>(range)) + 1);
   }
 
   std::println("Number of fresh ingredients = {}", total);
